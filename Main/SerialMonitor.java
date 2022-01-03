3efdfb350cdb454583299950d4eb003097620765
Ziad Saoud <69092782+ZiadSaoud@users.noreply.github.com>
2022-01-03 10:42:13 +0200

    Create SerialMonitor.java

---
+++ Main/SerialMonitor.java
@@ -0,0 +1,13 @@
+package com.ziadsaoud;
+
+
+
+public class SerialMonitor  {
+	
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		App_frame frame = new App_frame();
+
+} 
+
+}
